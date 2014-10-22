28db0448da48b94402932f4cddd9d9ed9af8a611
n434p <nazar.kolomiets@gmail.com>
2014-10-22 17:31:44 +0300

    added FractionsCalc

--- HOMEWORK/FractionsCalc/FractionsCalc/Program.cs
+++ HOMEWORK/FractionsCalc/FractionsCalc/Program.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace FractionsCalc
 {
     class Program
     {
+        class Fraction
+        {
+            int numerator;
+            int denumerator;
+            int intPart;
+        }
+
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter some string: \n");
+            string str = Console.ReadLine();
+            //Console.WriteLine("{0} {1} {2} {3}",p, Console.BufferWidth, Console.In, Console.Out);
+            Console.WriteLine("\n Edit your string: \n");
+            Console.Write(str);
+            foreach (var item in str)
+            {
+                Console.Write(item);
+            }
+            Console.SetCursorPosition(0, Console.CursorTop);
+            str  = Console.ReadLine();
         }
     }
 }
