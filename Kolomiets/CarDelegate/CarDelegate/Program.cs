0225fd63ad4bd1e8f1198b9f26da8b43614855b0
AndrewKulichenko <kulichenko.a@gmail.com>
2014-12-27 12:48:43 +0200

    dele

--- Kolomiets/CarDelegate/CarDelegate/Program.cs
+++ Kolomiets/CarDelegate/CarDelegate/Program.cs
@@ -6,12 +6,33 @@ using System.Threading.Tasks;
 
 namespace CarDelegate
 {
+    public class CarEventArgs : EventArgs
+    {
+        public readonly string msg;
+        public CarEventArgs(string message)
+        {
+            msg = message;
+        }
+    }
+
     class Program
     {
+        
+
         public class Car
         {
-        public delegate void CarEngineHandler(string msgForCaller);
-               private CarEngineHandler listOfHandlers;        public void RegisterWithCarEngine(CarEngineHandler methodToCall)
+        //public delegate void CarEngineHandler(string msgForCaller);
+        public delegate void CarEngineHandler(object obj, CarEventArgs e);
+
+        //public event CarEngineHandler Exploded;
+        //public event CarEngineHandler ToBlow;
+
+        public event EventHandler<CarEventArgs> ToBlow;
+        public event EventHandler<CarEventArgs> Exploded;
+
+        # region Car Body;
+
+        private CarEngineHandler listOfHandlers;        public void RegisterWithCarEngine(CarEngineHandler methodToCall)
         {
         listOfHandlers += methodToCall;
         }
@@ -32,23 +53,30 @@ namespace CarDelegate
         CurrentSpeed = currSp;
         MaxSpeed = maxSp;
         PetName = name;
+        
         }
+
+        public override string ToString()
+        {
+            return "Car: "+PetName;
+        }
+        # endregion
 
         public void Accelerate(int delta)
         {
         // Если этот автомобиль сломан,  отправить сообщение об этом, 
         if  (carlsDead)
         {
-        if  (listOfHandlers != null)
-        listOfHandlers ("Sorry,  this car is dead...");
+        if  (Exploded != null)
+            Exploded(this, new CarEventArgs("Car is dead!!!"));
         }
         else        {
         CurrentSpeed += delta;
         // Автомобиль почти сломан?
         if  (10 >=  (MaxSpeed - CurrentSpeed)
-        && listOfHandlers  != null)
+        && ToBlow != null)
         {
-        listOfHandlers("Careful buddy! Gonna blow!");
+        ToBlow(this,new CarEventArgs("Gonna to blow!!!"));
         }
         if  (CurrentSpeed >= MaxSpeed) 
         carlsDead = true; 
@@ -58,35 +86,45 @@ namespace CarDelegate
         }
         }
 
-        public static void OnCarEngineEvent(string msg)
+        public static void OnCarEngineEvent(object obj, CarEventArgs e)
         {
             Console.WriteLine("\n***** Message From Car Object *****");
-            Console.WriteLine("=> {0}", msg);
-         //   Console.WriteLine("*************************************");
-        }
-
-        public static void TestEngineEvent(string msg)
-        {
-            
-            Console.WriteLine("\n"); 
-            Console.WriteLine("To upper!!!");
+            Console.WriteLine("=> {0}", e.msg);
             Console.WriteLine("*************************************");
         }
+
+        //public static void TestEngineEvent(string msg)
+        //{
+            
+        //    Console.WriteLine("\n"); 
+        //    Console.WriteLine("To upper!!!");
+        //    Console.WriteLine("*************************************");
+        //}
 
         static void Main(string[] args)
         {
         Console.WriteLine("***** Delegates as event enablers *****\n");
-        // Сначала создать объект Car.
         Car cl = new Car ("SlugBug",  100,  10);
-        // Теперь сообщить ему, какой метод вызывать,
-        // когда он захочет отправить сообщение.
+
+        //cl.Exploded += new Car.CarEngineHandler(OnCarEngineEvent);
+        //cl.ToBlow += new Car.CarEngineHandler(OnCarEngineEvent);
+
+        // cl.Exploded -= OnCarEngineEvent;
+
+        cl.ToBlow += delegate (object ob, CarEventArgs e) { Console.WriteLine("Annonymous dlegate working {0} - {1}",Convert.ToString((Car) ob), e.msg); };
+        cl.Exploded += OnCarEngineEvent;
+
+        //EventHandler<CarEventArgs> d = new EventHandler<CarEventArgs>(OnCarEngineEvent);
+        //cl.Exploded += d;
+
         cl.RegisterWithCarEngine(new Car.CarEngineHandler(OnCarEngineEvent));
-        cl.RegisterWithCarEngine(new Car.CarEngineHandler(TestEngineEvent));
-        cl.UnRegisterWithCarEngine(new Car.CarEngineHandler(TestEngineEvent));
-        // Ускорить (это инициирует события) .
+
+        //cl.RegisterWithCarEngine(new Car.CarEngineHandler(TestEngineEvent));
+        //cl.UnRegisterWithCarEngine(new Car.CarEngineHandler(TestEngineEvent));
+
         Console.WriteLine ("***** Speeding up *****"); 
-        for  (int i = 0;  i < 9;  i++) 
-        cl.Accelerate(12);
+        for  (int i = 0;  i < 6;  i++) 
+        cl.Accelerate(20);
         Console.ReadLine();
         
         
