5ffffe5bf6c164b42283b89d605a32cf642b7af0
n434p <nazar.kolomiets@gmail.com>
2015-01-15 16:38:54 +0200

    150115-not

--- HOMEWORK/Parcer/Parcer/Program.cs
+++ HOMEWORK/Parcer/Parcer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -7,27 +8,50 @@ using System.Threading.Tasks;
 
 namespace Parcer
 {
+    class Test 
+    {
+        public void M(object ob) 
+        {
+            //string str = Console.InputEncoding.GetString();
+            //Console.WriteLine(str);
+        }
+    }
     class Program
     {
+
         static void Main(string[] args)
         {
-            string str = "23.16.15 3.45  5.6.8 ee15.2tt 65.e45e 44e.11 11.88";
-            Regex rx = new Regex(@"(?<!\.\k<first>)(?<number>(?<first>[\d]+)\.[\d]+)(?!\.)");
-            MatchCollection mc = rx.Matches(str);
-            foreach (Match item in mc)
+
+            //string str = "23.16.15 3.45  5.6.8 ee15.2tt 65.e45e 44e.11 11.88";
+            //Regex rx = new Regex(@"(?<!\.)(?<number>[\d]+\.[\d]+)(?!\.)");
+            //MatchCollection mc = rx.Matches(str);
+            //foreach (Match item in mc)
+            //{
+            //    Console.WriteLine(item.Groups["first"].Value);
+            //   Console.WriteLine(item.Groups["number"].Value);
+            //   Console.WriteLine("***********");
+            //}
+
+            //foreach (var item in rx.GetGroupNames())
+            //{
+            //    Console.WriteLine(item );
+            //}
+
+            //Console.WriteLine();
+            //Console.ReadKey();
+
+            using(Stream st = Console.OpenStandardInput())
             {
-                Console.WriteLine(item.Groups["first"].Value);
-               Console.WriteLine(item.Groups["number"].Value);
-               Console.WriteLine("***********");
+                StreamReader sr = new StreamReader(st);
+                
+                Console.Write("\n\n**********"+Convert.ToChar(sr.Read()));
+                Console.Write("\n\n**********" + Convert.ToChar(sr.Read()));
+                Console.Write("\n\n**********" + Convert.ToChar(sr.Read()));
+                Console.Write("\n\n**********" + Convert.ToChar(sr.Read()));
+                Console.Write("\n\n**********" + Convert.ToChar(sr.Read()));
             }
 
-            foreach (var item in rx.GetGroupNames())
-            {
-                Console.WriteLine(item );
-            }
 
-            Console.WriteLine();
-            Console.ReadKey();
         }
     }
 }
