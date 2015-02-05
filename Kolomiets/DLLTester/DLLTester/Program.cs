b923703370e54b37c3ae643c152052ae41989369
AndrewKulichenko <kulichenko.a@gmail.com>
2015-02-05 21:10:14 +0200

    sdfsdf

---
+++ Kolomiets/DLLTester/DLLTester/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLLTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            testGGGG t = new testGGGG(12, "asd", true);
+            t.Show();
+          
+            Console.ReadKey();
+
+        }
+    }
+}
