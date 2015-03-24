0b511c9e816e883d6b4fb33fe84b713a12b45c41
AndrewKulichenko <kulichenko.a@gmail.com>
2015-03-24 20:44:50 +0200

    adsasd

---
+++ Kolomiets/SQLCompactProj/SQLCompactProj/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SQLCompactProj
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TestContext db = new TestContext();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+           
+            People p = new People() { Name = tbName.Text, Age = Convert.ToInt32(tbAge.Text) };
+            tbName.Clear();
+            tbAge.Clear();
+            db.Peoples.Add(p);
+            db.SaveChanges();
+            lb.ItemsSource = db.Peoples.ToList();
+           
+        }
+    }
+}
---
+++ Kolomiets/SQLCompactProj/SQLCompactProj/TestContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLCompactProj
+{
+    class TestContext: DbContext
+    {
+        public TestContext() : base("Test")
+        { }
+
+        public DbSet<People> Peoples {get;set;}
+    }
+
+    class People
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return Name + " " + Age;
+        }
+    }
+}
