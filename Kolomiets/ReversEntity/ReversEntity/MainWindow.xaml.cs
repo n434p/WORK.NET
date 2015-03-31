398532c36c9b6af3835e8e427dc0b5650d2bced1
AndrewKulichenko <kulichenko.a@gmail.com>
2015-03-31 20:50:30 +0300

    asdfdsfswdf

---
+++ Kolomiets/ReversEntity/ReversEntity/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+
+namespace ReversEntity
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            string str = ConfigurationManager.ConnectionStrings["Example"].ConnectionString;
+            ModelContainer db = new ModelContainer();  
+            Departments d = new Departments(){Id_dep = 1, Name = "asdasd"};
+            db.DepartmentsSet.Add(d);
+            db.SaveChanges();
+            lb.ItemsSource = (from dep in db.DepartmentsSet select dep.Name).ToList();
+        }
+    }
+}
---
+++ Kolomiets/WPFEntity/WPFEntity/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFEntity
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        library db = new library();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            dgMain.ItemsSource = db.Categories.ToList();
+            dgMain.Columns[2].Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void dgMain_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            
+            MessageBox.Show((dgMain.CurrentCell.Item as Categories).Id.ToString());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Categories c = new Categories() { Name = tbName.Text };
+            db.Categories.Add(c);
+            db.ChangeTracker.DetectChanges();
+            db.SaveChanges();
+            dgMain.
+            tbName.Text = "";
+            
+           // dgMain.ItemsSource = db.Categories.ToList();
+        }
+    }
+}
