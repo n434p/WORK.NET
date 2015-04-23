2b24357f0cb6608727a0c80c2c9bd476f2c12210
AndrewKulichenko <kulichenko.a@gmail.com>
2015-04-23 20:53:28 +0300

    asdasd

---
+++ HOMEWORK/HTML/JS/jscript.js
@@ -0,0 +1,16 @@
+﻿//first.click = f;
+//second.click = f;
+//third.click = f;
+
+first.i = 0;
+second.i = 0;
+third.i = 0;
+
+var color = ["brown", "aqua", "azure", "blue", "pink"];
+
+function f(ob)
+{
+    
+    ob.style.backgroundColor = color[++this.i];
+    alert("1");
+}
--- HOMEWORK/HTML/JS/script.js
+++ HOMEWORK/HTML/JS/script.js
@@ -2,20 +2,25 @@
 function T()
 {
     alert("Take a number from 1 to 100...");
-    var start = 0, end = 100, cur;
+    var start = 0, end = 100, cur, check;
 
     while(true)
     {
         cur=Math.round((end-start)/2)+start;
         res = confirm("Is your number less than " + cur + " ?");
+        
+
+        if (check == cur) break;
 
         if (end - start == 1) {
+            alert("1 "+cur);
             if (res) { cur = end; }
             else { cur = start; }
             break;
         }
 
         if (start - end == 1) {
+            alert("2 "+cur);
             if (res) { cur = start; }
             else { cur = end; }
             break;
@@ -25,7 +30,21 @@ function T()
         else
         { start = cur; }
 
-        
+        check = cur;
+
     }
+
     alert("Your number is - " +cur+"\n\n*** our friendship site: www.badcheater.com");
+}
+
+
+function N()
+{
+    var arr = new Array(8);
+    for (var i = 0; i < arr.length; i++)
+    {
+        arr[i] = i+1;
+    }
+    document.write(arr.join(","));
+   
 }
