1106b63c2c2cb9dc2c8f5dfc6d03cef2528b2ef8
n434p <nazar.kolomiets@gmail.com>
2015-04-25 02:08:39 +0300

    asdas

--- HOMEWORK/HTML/JS/jscript.js
+++ HOMEWORK/HTML/JS/jscript.js
@@ -1,16 +1,31 @@
-﻿//first.click = f;
-//second.click = f;
-//third.click = f;
-
-first.i = 0;
-second.i = 0;
-third.i = 0;
-
-var color = ["brown", "aqua", "azure", "blue", "pink"];
+﻿
+var color = ["red", "green", "blue", "yellow", "orange"];
+var r = [];
 
 function f(ob)
 {
-    
-    ob.style.backgroundColor = color[++this.i];
-    alert("1");
+    var curColor = ob.style.backgroundColor;
+    var cur = -1;
+    switch (ob.id)
+    {
+        case "first": cur = 0;
+            break;
+        case "second": cur = 1;
+            break;
+        case "third": cur = 2;
+            break;
+    }
+
+    if (color.indexOf(curColor) < color.length-1 && color.indexOf(curColor) >= 0)
+    { r[cur] = color.indexOf(curColor)+1; }
+    else
+    {
+        r[cur] = 0;
+    }
+
+    console.log(color.indexOf(curColor));
+    ob.style.backgroundColor = color[r[cur]];
+    ob.children[0].innerHTML = color[r[cur]];
+    console.log(r);
+
 }
