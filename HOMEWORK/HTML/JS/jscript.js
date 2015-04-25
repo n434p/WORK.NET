8f99c989ae128cf8a72a739ca6ef7270eb3a3719
AndrewKulichenko <kulichenko.a@gmail.com>
2015-04-25 12:47:44 +0300

    asdasd

--- HOMEWORK/HTML/JS/jscript.js
+++ HOMEWORK/HTML/JS/jscript.js
@@ -2,30 +2,43 @@
 var color = ["red", "green", "blue", "yellow", "orange"];
 var r = [];
 
+function Rectangle(id,color, counter)
+{
+    this.id = id;
+    this.color = color;
+    this.counter = counter;
+}
+
 function f(ob)
 {
-    var curColor = ob.style.backgroundColor;
-    var cur = -1;
-    switch (ob.id)
-    {
-        case "first": cur = 0;
-            break;
-        case "second": cur = 1;
-            break;
-        case "third": cur = 2;
-            break;
-    }
+    rect = new Rectangle(ob.id, ob.style.backgroundColor, 0);
+    ob.style.backgroundColor = color[rect.counter];
+    r.push(rect);
+    
+    
 
-    if (color.indexOf(curColor) < color.length-1 && color.indexOf(curColor) >= 0)
-    { r[cur] = color.indexOf(curColor)+1; }
-    else
-    {
-        r[cur] = 0;
-    }
+    //var curColor = ob.style.backgroundColor;
+    //var cur = -1;
+    //switch (ob.id)
+    //{
+    //    case "first": cur = 0;
+    //        break;
+    //    case "second": cur = 1;
+    //        break;
+    //    case "third": cur = 2;
+    //        break;
+    //}
 
-    console.log(color.indexOf(curColor));
-    ob.style.backgroundColor = color[r[cur]];
-    ob.children[0].innerHTML = color[r[cur]];
-    console.log(r);
+    //if (color.indexOf(curColor) < color.length-1 && color.indexOf(curColor) >= 0)
+    //{ r[cur] = color.indexOf(curColor)+1; }
+    //else
+    //{
+    //    r[cur] = 0;
+    //}
+
+    //console.log(color.indexOf(curColor));
+    //ob.style.backgroundColor = color[r[cur]];
+    //ob.children[0].innerHTML = color[r[cur]];
+    //console.log(r);
 
 }
