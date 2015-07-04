77618e114477b85b25f4e2c56ea6564f18d5ca66
n434p <nazar.kolomiets@gmail.com>
2015-07-04 03:19:23 +0300

    SAVEPOINT

---
+++ HOMEWORK/Patterns/Command/Command/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create user and let her compute
+            User user = new User();
+            // User presses calculator buttons
+            user.Compute('+', 100);
+            user.Compute('-', 50);
+            user.Compute('*', 10);
+            user.Compute('/', 2);
+            // Undo 4 commands
+            user.Undo(4);
+            // Redo 3 commands
+            user.Redo(3);
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
+
+
+/// <summary>
+/// The 'Command' abstract class
+/// </summary>
+abstract class Command
+{
+    public abstract void Execute();
+    public abstract void UnExecute();
+}
+
+
+/// <summary>
+/// The 'ConcreteCommand' class
+/// </summary>
+class CalculatorCommand : Command
+{
+    private char operation;
+    private int operand;
+    private Calculator calculator;
+
+    // Constructor
+    public CalculatorCommand(Calculator calculator, char operation, int operand)
+    {
+        this.calculator = calculator;
+        this.operation = operation;
+        this.operand = operand;
+    }
+
+    // Gets operator
+    public char Operator { set { operation = value; }}
+
+    // Get operand
+    public int Operand {set { operand = value; } }
+
+    // Execute new command
+    public override void Execute()
+    {
+        calculator.Operation(operation, operand);
+    }
+
+    // Unexecute last command
+    public override void UnExecute()
+    {
+        calculator.Operation(Undo(operation), operand);
+    }
+
+    // Returns opposite operator for given operator
+    private char Undo(char operation)
+    {
+        switch (operation)
+        {
+            case '+': return '-';
+            case '-': return '+';
+            case '*': return '/';
+            case '/': return '*';
+            default: throw new ArgumentException("unknown operation");
+        }
+
+    }
+
+}
+
+
+/// <summary>
+/// The 'Receiver' class
+/// </summary>
+class Calculator
+{
+    private int curValue = 0;
+
+    public void Operation(char operation, int operand)
+    {
+        switch (operation)
+        {
+            case '+': curValue += operand; break;
+            case '-': curValue -= operand; break;
+            case '*': curValue *= operand; break;
+            case '/': curValue /= operand; break;
+        }
+
+        Console.WriteLine("Current value = {0,3} (operation {1} {2})", curValue, operation, operand);
+
+    }
+
+}
+
+
+
+/// <summary>
+/// The 'Invoker' class
+/// </summary>
+class User
+{
+    // Initializers
+    private Calculator calculator = new Calculator();
+    private List<Command> commandsList = new List<Command>();
+    private int current = 0;
+
+
+    public void Redo(int levels)
+    {
+        Console.WriteLine("\n---- Redo {0} levels ", levels);
+
+        // Perform redo operations
+        for (int i = 0; i < levels; i++)
+        {
+            if (current < commandsList.Count - 1)
+            {
+                Command command = commandsList[current++] as Command; 
+                command.Execute();
+            }
+        }
+
+    }
+
+
+    public void Undo(int levels)
+    {
+        Console.WriteLine("\n---- Undo {0} levels ", levels);
+        // Perform undo operations
+        for (int i = 0; i < levels; i++)
+        {
+            if (current > 0)
+            {
+                Command command = commandsList[--current] as Command;
+                command.UnExecute();
+            }
+        }
+
+    }
+
+
+    public void Compute(char operation, int operand)
+    {
+        // Create command operation and execute it
+        Command command = new CalculatorCommand(calculator, operation, operand);
+        command.Execute();
+
+        // Add command to undo list
+        commandsList.Add(command);
+        current++;
+    }
+
+}
+
+
+
+
+
+
---
+++ HOMEWORK/Patterns/Mediator/Mediator/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManagerMediator mediator = new ManagerMediator();
+            Colleague customer = new CustomerColleague(mediator);
+            Colleague constructor = new ConstructionColleague(mediator);
+            Colleague post = new PostColleague(mediator);
+            mediator.Customer = customer;
+            mediator.Constructor = constructor;
+            mediator.Post = post;
+            customer.Send("Customer have new order for the factory."); // Factory can construct ordered unit
+            constructor.Send("Factory done his work."); // Unit construction is complete. Post service can bring unit to the customer
+            post.Send("Unit was delivered"); // Customer can get unit
+
+            Console.Read();
+        }
+    }
+
+    abstract class Mediator
+    {
+        public abstract void Send(string msg, Colleague colleague);
+    }
+    abstract class Colleague
+    {
+        protected Mediator mediator;
+
+        public Colleague(Mediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public virtual void Send(string message)
+        {
+            mediator.Send(message, this);
+        }
+        public abstract void Notify(string message);
+    }
+
+    class CustomerColleague : Colleague
+    {
+        public CustomerColleague(Mediator mediator)
+            : base(mediator)
+        { }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine("To the customer: " + message);
+        }
+    }
+
+    class ConstructionColleague : Colleague
+    {
+        public ConstructionColleague(Mediator mediator)
+            : base(mediator)
+        { }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine("To the factory: " + message);
+        }
+    }
+
+    class PostColleague : Colleague
+    {
+        public PostColleague(Mediator mediator)
+            : base(mediator)
+        { }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine("To the post service: " + message);
+        }
+    }
+
+    class ManagerMediator : Mediator
+    {
+        public Colleague Customer { get; set; }
+        public Colleague Constructor { get; set; }
+        public Colleague Post { get; set; }
+        public override void Send(string msg, Colleague colleague)
+        {
+            // if sender is customer - send message to the factory
+            // etc. There is some order to construct unit
+            if (Customer == colleague)
+                Constructor.Notify(msg);
+            // if sender is factory - send message to the post service
+            // etc. Unit is complete needed to deliver
+            else if (Constructor == colleague)
+                Post.Notify(msg);
+            // at least if sender is post service - send message to the customer
+            else if (Post == colleague)
+                Customer.Notify(msg);
+        }
+    }
+
+
+}
